65ec40779d02408985411b782eb0aa4f18f3109e
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-22 13:51:55 +0000

    Added new faction ships and improved formatting

--- ships.js
+++ ships.js
@@ -1,14 +1,25 @@
 var ships = {
+	// Blops
 	"Panther": 0.25,
 	"Redeemer": 0.25,
 	"Sin": 0.25,
 	"Widow": 0.25,
+
+	// Industrials
 	"Rorqual": 0.1,
 	"Rorqual ORE Development Edition": 0.1,
+	"Anshar": 0.1,
+	"Ark": 0.1,
+	"Nomad": 0.1,
+	"Rhea": 0.1,
+
+	// Carriers
 	"Archon": 1.0,
 	"Chimera": 1.0,
 	"Nidhoggur": 1.0,
 	"Thanatos": 1.0,
+
+	// Dreads
 	"Moros": 1.0,
 	"Moros Interbus Edition": 1.0,
 	"Naglfar": 1.0,
@@ -17,19 +28,36 @@ var ships = {
 	"Phoenix Wiyrkomi Edition": 1.0,
 	"Revelation": 1.0,
 	"Revelation Sarum Edition": 1.0,
-	"Anshar": 0.1,
-	"Ark": 0.1,
-	"Nomad": 0.1,
-	"Rhea": 0.1,
-	"Jump Bridge": 1.0,
-	"QA Jump Bridge": 1.0,
+	"Vehement": 1.0,
+	"Chemosh": 1.0,
+	"Caiman": 1.0,
+
+	// FAXes
+	"Apostle": 1.0,
+	"Minokawa": 1.0,
+	"Lif": 1.0,
+	"Ninazu": 1.0,
+	"Loggerhead": 1.0,
+	"Dagon": 1.0,
+
+	// Supers
 	"Aeon": 1.0,
 	"Hel": 1.0,
 	"Nyx": 1.0,
 	"Revenant": 1.0,
 	"Wyvern": 1.0,
+	"Vendetta": 1.0,
+
+	// Titans
 	"Avatar": 1.0,
 	"Erebus": 1.0,
 	"Leviathan": 1.0,
-	"Ragnarok": 1.0
+	"Ragnarok": 1.0,
+	"Vanquisher": 1.0,
+	"Molok": 1.0,
+	"Komodo": 1.0,
+
+	// Infrastructure
+	"Jump Bridge": 1.0,
+	"QA Jump Bridge": 1.0,
 }
